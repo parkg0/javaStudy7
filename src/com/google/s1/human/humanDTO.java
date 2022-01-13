5efe896122f7665592aa9d18391f75ef4a51e5d4
parkg0 <wldud9883@naver.com>
2022-01-13 16:33:47 +0900

    call

---
+++ src/com/google/s1/human/humanDTO.java
@@ -0,0 +1,45 @@
+package com.google.s1.human;
+
+public class humanDTO {
+
+	private String name;
+	private String nickname;
+	private String email;
+	private String number;
+	private String birth;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	
+	
+	
+}
---
+++ src/com/google/s1/human/humanData.java
@@ -0,0 +1,5 @@
+package com.google.s1.human;
+
+public class humanData {
+
+}
